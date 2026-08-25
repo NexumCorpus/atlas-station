813c81b1b7e82628885e5ac57b2237ce6fe02e04
NexumCorpus <nexumcorpus@gmail.com>
2026-08-25 16:56:22 -0400

    WIP checkpoint (B-309)

---
+++ fix.cjs
@@ -0,0 +1,6 @@
+﻿const fs=require('fs');
+let p=fs.readFileSync('patch-retry.cjs','utf8');
+p=p.replace("s = s.replace(a4, ([", "s = s.replace(a4, lit([");
+p=p.replace("}).join(NL) + NL + '      ' + a4);", "].join('\n')) + NL + '      ' + a4);");
+fs.writeFileSync('patch-retry.cjs',p);
+console.log('ok2');
--- patch-retry.cjs
+++ patch-retry.cjs
@@ -27,7 +27,7 @@ s = s.replace(a3, lit([
 // 3) retry-success fact append at terminalization
 let a4 = lit("if (_memstore && _memstore.recordTerminalOnce(id)) try { _memstore.appendRun({ agentId: id, task: agents.get(id)?.task,");
 if (!s.includes(a4)) throw new Error('anchor4 miss');
-s = s.replace(a4, lit([
+s = s = s.replace(a4, lit([
 'try {',
 '        const __rec = agents.get(id) || {};',
 '        if (__rec.retryOf && done && _memstore) {',
@@ -36,7 +36,7 @@ s = s.replace(a4, lit([
 "            send('retry_success_logged', { id, retryOf: __rec.retryOf });",
 '          } catch (_) {}',
 '        } // on repeat failure, failSubtype is already recorded via set() above',
-'      } catch (_) {}') + NL + '      ' + a4);
+'      } catch (_) {}').join(NL) + NL + '      ' + a4);
 
 fs.writeFileSync('fleethost.mjs', s);
 console.log('patched ok');
