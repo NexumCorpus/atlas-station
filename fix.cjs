0964a157340179477c30ee2296a79acd43fba702
NexumCorpus <nexumcorpus@gmail.com>
2026-08-25 17:09:22 -0400

    chore: remove B-309 patch scaffolding scripts

--- fix.cjs
+++
@@ -1,4 +0,0 @@
-﻿const fs=require('fs');
-let lines=fs.readFileSync('patch-retry.cjs','utf8').split('\n');
-lines.splice(38,1); // remove stray leftover line
-fs.writeFileSync('patch-retry.cjs',lines.join('\n'));
